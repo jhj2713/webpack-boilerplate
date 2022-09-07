7839bc0b1982dfd085e7a9970b0c91ab4ead5a2a
juhyojeong <juhyojeong58@gmail.com>
2022-09-07 21:26:30 +0900

    feat: webpack 설정

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import "./main.scss";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const root = document.querySelector("#root");
+  root.innerHTML = `<h1>ROOT</h1>`;
+});
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  mode: "development",
+  entry: {
+    main: "./src/app.js",
+  },
+  output: {
+    path: path.resolve("./dist"),
+    filename: "[name].js",
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.s?[ac]ss/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              publicPath: "../dist/",
+              name: "[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
